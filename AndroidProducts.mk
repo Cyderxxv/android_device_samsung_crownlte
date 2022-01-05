9ecd6961374a68a787fbe32d72fc7ff007555df0
CyderXXV <87647653+CyderVII28x03@users.noreply.github.com>
2022-01-05 10:27:44 +0700

    DerpFest 12 Bringup
    Initial commit for DerpFest12

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_crownlte.mk
+    $(LOCAL_DIR)/derp_crownlte.mk
 
 COMMON_LUNCH_CHOICES := \
-     aosp_crownlte-userdebug \
-     aosp_crownlte-eng \
-     aosp_crownlte-user
+     derp_crownlte-userdebug \
+     derp_crownlte-eng \
+     derp_crownlte-user
